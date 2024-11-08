15a8acc37ffc91ffe98d2151c5ad69e4855d8ce7
Pranav <pvanparty@gmail.com>
2024-11-07 20:00:39 -0500

    Started spin command

--- src/main/java/frc/robot/commands/Spin.java
+++ src/main/java/frc/robot/commands/Spin.java
@@ -5,11 +5,23 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.DriveTrain;
 
 public class Spin extends Command {
   /** Creates a new Spin. */
-  public Spin() {
+  private DriveTrain m_driveTrain;
+  public double m_duration;
+  public double m_speed;
+  public double m_startTime;
+  public double m_rotation;
+
+  // Todo: add a way to make it spin for a set amount of time (ie 5 seconds because we dont know the angle of the robot hence we have to rely on time)
+
+
+  public Spin(DriveTrain thDriveTrain) {
     // Use addRequirements() here to declare subsystem dependencies.
+    m_driveTrain = thDriveTrain;
+    addRequirements(m_driveTrain);
   }
 
   // Called when the command is initially scheduled.
@@ -18,7 +30,9 @@ public class Spin extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_driveTrain.arcadeDrive(m_speed, m_rotation);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
